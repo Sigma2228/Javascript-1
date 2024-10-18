42987d84302bbdf9483f24bf69b27989bcf9d86d
Sigma2228 <149525498+Sigma2228@users.noreply.github.com>
2024-10-18 12:33:19 +0300

    commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+const age = 14;
+console.log(age)
+const name = "Nazar"
+console.log(name)
+const isStudent = confirm('You is student?')
+console.log(isStudent)
+const myString = ('sgshdggdhsggdhg')
+console.log(myString)
+const myNumber = 25;
+a = myNumber + 10;
+console.log(a)
+const myNull = null;
+console.log(myNull)
+const yourName = prompt('Your Name')
+if (yourName) {
+    alert("Привіт, " + yourName + "!")}
+    else {
+        alert("Ви не ввели ім'я!");
+    }
+    const sure = confirm('Підтвердіть')
+    if (sure) {
+        alert("Дякую за підтвердження!")
+    }
+    else {
+        alert('Дію Відмінено')
+    }
+    const Alert = confirm('Дія Небезпечна')
+    if(Alert) {
+        alert('Дякую за підтвердження!')
+    }
+    else {
+        alert('Дію Відмінено')
+    }
